83c6651c74663cd52f05fdf75dd502a5fc5d656e
jhemesferreira <jhemesferreira.dla@gmail.com>
2020-03-01 12:00:52 -0300

    passar'

---
+++ projeto_agenda/contatos/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Contato
+from django.http import  Http404
+from django.core.paginator import Paginator
+
+def index(request):
+    contatos = Contato.objects.order_by('nome')
+    paginator = Paginator(contatos, 3)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return  render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
+
+def ver_contato(request, contato_id):
+    contato = get_object_or_404(Contato, id = contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
+    return  render(request, 'contatos/ver_contato.html', {
+        'contato': contato
+    })
